febf6cd3c4df5415c0828ceb8e1e42dc12e87ed4
mateo <m.devia@uniandes.edu.co>
2019-10-30 17:59:16 -0500

    console debuging

--- MyMongoLib.js
+++ MyMongoLib.js
@@ -42,6 +42,7 @@ const MyMongoLib = function() {
 
       // Insert a single document
       const testCol = db.collection("pedidosReactive");
+      console.log("encontro la coleccion");
       const csCursor = testCol.watch();
       csCursor.on("change", data => {
         console.log("changed", data);
