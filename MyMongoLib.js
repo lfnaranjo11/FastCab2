10e8bbe94c8e18068103263e3c69ee7d07ddc9c4
mateo <m.devia@uniandes.edu.co>
2019-10-30 16:56:12 -0500

    env variable updated

--- MyMongoLib.js
+++ MyMongoLib.js
@@ -2,7 +2,7 @@ const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
 
 // Connection URL
-const url = process.env.MONGO_URL || "mongodb://localhost:27017";
+const url = process.env.PROD_MONGODB || "mongodb://localhost:27017";
 
 // Database Name
 const dbName = "reactive";
