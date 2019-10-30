57dbcbac67bc747c8aba4ca626ce4a6b263a2033
mateo <m.devia@uniandes.edu.co>
2019-10-30 17:03:25 -0500

    build folder added

---
+++ front/build/precache-manifest.9ca73d5a578146a2ad0457237c3919cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b22ccbd148066bc77c903d7de96fc7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55e8b5c5ec0b0427c5e2",
+    "url": "/static/css/main.445b3572.chunk.css"
+  },
+  {
+    "revision": "14a4da5d3b229d3d016c",
+    "url": "/static/js/2.cd4c04be.chunk.js"
+  },
+  {
+    "revision": "55e8b5c5ec0b0427c5e2",
+    "url": "/static/js/main.4581679a.chunk.js"
+  },
+  {
+    "revision": "ebd3a683af0fa2dbb56f",
+    "url": "/static/js/runtime-main.7139b951.js"
+  }
+]);
