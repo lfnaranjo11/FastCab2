a90184d15fc3687b7a9dcb11b7abca3ab8ec1a23
mateo <m.devia@uniandes.edu.co>
2019-11-01 10:02:12 -0500

    new response

--- routes/twilio.js
+++ routes/twilio.js
@@ -4,7 +4,7 @@ const MessagingResponse = require("twilio").twiml.MessagingResponse;
 
 router.post("/newmessage", (req, res) => {
   const twiml = new MessagingResponse();
-  twiml.message(twiml.toString());
+  twiml.message(req.body);
   res.writeHead(200, { "Content-Type": "text/xml" });
   res.end(twiml.toString());
 });
