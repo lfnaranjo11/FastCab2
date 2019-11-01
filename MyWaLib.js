7a5a81473f1a2296265d44eb11d9301f881e0427
mateo <m.devia@uniandes.edu.co>
2019-11-01 10:44:42 -0500

    new MyWaLib organization try

--- MyWaLib.js
+++ MyWaLib.js
@@ -1,6 +1,7 @@
 const accountSid = process.env.TWILIO_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
 
 const MyWaLib = function() {
   const MyWaLib = this || {};
@@ -12,6 +13,13 @@ const MyWaLib = function() {
         to: "whatsapp:+573004654173"
       })
       .then(message => console.log(message.sid));
+  };
+
+  MyWaLib.receiveMessage = newMessage => {
+    const twiml = new MessagingResponse();
+    twiml.message("Bienvenido " + newMessage);
+    res.writeHead(200, { "Content-Type": "text/xml" });
+    res.end(twiml.toString());
   };
 
   return MyWaLib;
--- routes/twilio.js
+++ routes/twilio.js
@@ -1,13 +1,17 @@
 var express = require("express");
 var router = express.Router();
+var MyWaLib = require("../MyWaLib");
 const MessagingResponse = require("twilio").twiml.MessagingResponse;
 
 router.post("/newmessage", (req, res) => {
   const newMessage = req.body.Body;
+  /**
   const twiml = new MessagingResponse();
-  twiml.message(JSON.stringify(req.body));
+  twiml.message("Bienvenido " + newMessage);
   res.writeHead(200, { "Content-Type": "text/xml" });
   res.end(twiml.toString());
+  */
+  MyWaLib.receiveMessage(newMessage);
 });
 
 module.exports = router;
