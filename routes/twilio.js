9253761e9f1ad12c52052d22edea1b27fd91fbec
mateo <m.devia@uniandes.edu.co>
2019-11-01 10:58:17 -0500

    message send to mongo try

--- routes/twilio.js
+++ routes/twilio.js
@@ -1,17 +1,17 @@
 var express = require("express");
 var router = express.Router();
 var MyWaLib = require("../MyWaLib");
+var MyMongoLib = require("../MyMongoLib");
 const myWaLib = MyWaLib();
+const myMongoLib = MyMongoLib();
 
 router.post("/newmessage", (req, res) => {
   const newMessage = req.body.Body;
-  /**
-  const twiml = new MessagingResponse();
-  twiml.message("Bienvenido " + newMessage);
-  res.writeHead(200, { "Content-Type": "text/xml" });
-  res.end(twiml.toString());
-  */
   myWaLib.receiveMessage(newMessage, res);
+  myMongoLib
+    .insertDocument(newMessage)
+    .then(() => res.send({ msg: "Inserto" }))
+    .catch(err => res.send({ err: true, msg: err }));
 });
 
 module.exports = router;
