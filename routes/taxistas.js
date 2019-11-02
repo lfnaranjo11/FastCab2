0d34efa7aaca485a51a75d36106d8623f475caa6
mateo <m.devia@uniandes.edu.co>
2019-11-02 10:04:52 -0500

    conductor info included

--- routes/taxistas.js
+++ routes/taxistas.js
@@ -7,7 +7,17 @@ const myMongoLib = MyMongoLib();
 
 router.post("/accept", (req, res) => {
   let viaje = req.body.viaje;
-  myWaLib.sendMessage("!Viaje Confirmado!", viaje.usuario);
+  let conductor = req.body.consuctor;
+  myWaLib.sendMessage(
+    `!Tu viaje hacia ${viaje.direccion} ha sido confirmado!
+    Tu conductor se llama ${conductos.nombre}.
+    A continuación encontrarás su información:
+    Cedula: ${conductor.cedula}
+    Placa del carro: ${conductor.placa}
+    Modelo del carro: ${conductor.carro}
+    `,
+    viaje.usuario
+  );
   myMongoLib
     .acceptViaje(viaje)
     .then(res.send("ok"))
