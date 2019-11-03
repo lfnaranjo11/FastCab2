3b4ab7eff7531a1ef94e207b3f95e5df7c85624b
mateo <m.devia@uniandes.edu.co>
2019-11-03 15:14:35 -0500

    generate jwt

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get("/data", (req, res) => {
     .getDocs()
     .then(docs => {
       console.log("docs");
-      res.send(docs);
+      res.send(req.headers.authorization);
     })
     .catch(err => res.send({ err: true, msg: err }));
 });
