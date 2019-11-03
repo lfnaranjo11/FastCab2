c6f7e85f056472900914116a3fe25099647c0100
mateo <m.devia@uniandes.edu.co>
2019-11-03 12:12:01 -0500

    create user try

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,5 +1,5 @@
 var express = require("express");
-const argon2 = require('argon2');
+const argon2 = require("argon2");
 var router = express.Router();
 var MyMongoLib = require("../MyMongoLib");
 const myMongoLib = MyMongoLib();
@@ -9,9 +9,14 @@ router.post("/create", (req, res) => {
   let contraseña = req.body.contraseña;
   let foto = req.body.foto;
   const salt = randomBytes(32);
-  const passwordHashed = await argon2.hash(contraseña, { salt });
+  const passwordHashed = argon2.hash(contraseña, { salt });
   myMongoLib
-    .createUser({usuario: usuario, contraseña: passwordHashed, foto: foto, salt: salt.toString('hex')})
+    .createUser({
+      usuario: usuario,
+      contraseña: passwordHashed,
+      foto: foto,
+      salt: salt.toString("hex")
+    })
     .then(() => res.send({ msg: "Creo un usuario" }))
     .catch(err => res.send({ err: true, msg: err }));
 });
